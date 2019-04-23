9c83a33d6e681841d4ae5e74c4431af7603e2efd
meiwenhua <meiwenhua@protonmail.com>
2019-04-23 05:44:30 -0400

    fix:crash when don't load hub.pkl

--- freeproxypool/hub.py
+++ freeproxypool/hub.py
@@ -2,9 +2,6 @@ from common import *
 import asyncio
 
 class Hub(dict):
-    def __init__(self):
-        self._lock = asyncio.Lock()
-
-    def __init__(self, _dict):
+    def __init__(self, _dict={}):
         self._lock = asyncio.Lock()
         super(Hub, self).__init__(_dict)
